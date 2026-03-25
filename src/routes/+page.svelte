b8418a76fc21f3686e34913fe8dd17ef1c462094
LetiziaCristalli <letizia.cristalli@mail.polimi.it>
2026-03-25 17:29:17 +0100

    add state and derived property

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,18 @@
 <script>
     console.log('Hello Wordl');
+    let count= $state(2);
+    let doubled = $derived (count * 2);
 </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={() => count++}>
+    clicks: {count}
+</button>
+
+<span>doubled is {doubled}</span>
+
 <style>
     h1  {
         color: deepskyblue;
