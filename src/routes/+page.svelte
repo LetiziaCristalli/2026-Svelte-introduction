764843a192ae21264a25c78147fc659584f9a772
LetiziaCristalli <letizia.cristalli@mail.polimi.it>
2026-04-08 18:09:12 +0200

    title editor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
 <script>
+    import {website} from "$lib/state.svelte"
+
     import ClickCounter from "$lib/components/ClickCounter.svelte"
     import CanvasSquare from "$lib/components/CanvasSquare.svelte"
     import Todolist from "$lib/components/todolist.svelte";
+    import TitleEditor from "$lib/components/TitleEditor.svelte";
 
     let count= $state(5);
     let double = $derived(count * 2);
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>{website.title}</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<TitleEditor />
 
 <ClickCounter bind:initialCount={count} />
 
