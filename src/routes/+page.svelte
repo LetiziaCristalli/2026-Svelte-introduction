c4c39eb2b307fa7472963fc68d4ec44b73840b7a
LetiziaCristalli <letizia.cristalli@mail.polimi.it>
2026-04-08 16:25:27 +0200

    $bindable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,15 +8,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button onclick={()=> count++}>
-    clicks:{count}
-</button>
-
-<span> doubled is {double} </span>
-
 <ClickCounter bind:initialCount={count} />
 <p>count from parent is {count}</p>
-
 
 <style>
     h1  {
