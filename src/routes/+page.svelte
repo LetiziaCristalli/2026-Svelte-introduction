053aa9bc4ae03cce60c38d57d84356494a29e058
LetiziaCristalli <letizia.cristalli@mail.polimi.it>
2026-04-08 15:19:42 +0200

    define and change $state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
 <script>
     import ClickCounter from "$lib/components/ClickCounter.svelte"
-    import ProgressiveSum from "$lib/components/ProgressiveSum.svelte"
+
+    let count= $state(5);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClickCounter />
-<ProgressiveSum />
+<button onclick={()=> count++}>
+    clicks:{count}
+</button>
+
+<ClickCounter bind:initialCount={count} />
+<p>count from parent is {count}</p>
+
 
 <style>
     h1  {
